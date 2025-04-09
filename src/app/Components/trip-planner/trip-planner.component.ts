b1518a2ee4e59aebd556077fde7d40ff0599494a
gaurav.davda <gauravdavda34@gmail.com>
2025-04-10 01:40:46 +0530

    feat: implement trip planner component and update app structure

---
+++ src/app/Components/trip-planner/trip-planner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-planner',
+  templateUrl: './trip-planner.component.html',
+  styleUrls: ['./trip-planner.component.scss']
+})
+export class TripPlannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TripPlannerComponent } from './Components/trip-planner/trip-planner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TripPlannerComponent
   ],
   imports: [
     BrowserModule,
