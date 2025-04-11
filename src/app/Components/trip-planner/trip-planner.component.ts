4a07a49820cc8b4b9b13bb14d6134d7b296ef77f
gaurav.davda <gauravdavda34@gmail.com>
2025-04-11 23:16:07 +0530

    feat: enhance trip planner with dynamic route graph and improved trip point management

--- src/app/Components/trip-planner/trip-planner.component.ts
+++ src/app/Components/trip-planner/trip-planner.component.ts
@@ -12,6 +12,11 @@ export class TripPlannerComponent implements OnInit {
     stPoint: new FormControl(''),
     endPoint: new FormControl(''),
   });
+  protected tripRoutesGraph: any[] = [];
+  cityX: { [city: string]: number } = {}; // To track positions
+  spacing = 200;
+  padding = 50;
+
 
   constructor() { }
 
@@ -25,9 +30,49 @@ export class TripPlannerComponent implements OnInit {
     return this.tripPlanForm.get('tripPoints') as FormArray;
   }
 
+  //Method : Add New Trip Routes
   protected addTripPoint(): void {
-    this.tripPoints.push(this.tripPointsTemplate);
-    console.info('Adding trip point', this.tripPlanForm.value);
+    this.tripPoints.push(new FormGroup({
+      stPoint: new FormControl(''),
+      endPoint: new FormControl(''),
+    }));
+  }
+
+  //Method : Create Trip Routes Graph
+  protected createTripRoutes(): void {
+    this.tripRoutesGraph = [];
+    const rawTrips = this.tripPlanForm.value.tripPoints || [];
+
+    for (let i = 0; i < rawTrips.length; i++) {
+      const { stPoint, endPoint } = rawTrips[i];
+      if (!stPoint || !endPoint) continue;
+      const { level, type } = this.idetifyRoutesType(stPoint, endPoint);
+      this.tripRoutesGraph.push({ stPoint, endPoint, level, type });
+    };
+    console.log('Trip Routes Graph:', this.tripRoutesGraph);
+    const cities = [...new Set(this.tripRoutesGraph.flatMap(t => [t.stPoint, t.endPoint]))];
+    cities.forEach((city, i) => this.cityX[city] = this.padding + i * this.spacing);
+  }
+
+  private idetifyRoutesType(stPoint: string, endPoint: string): { level: number; type: 'continued' | 'not-continued' | 'repeated' } {
+    const lastTripRoute = this.tripPoints.value[this.tripPoints.value.length - 1];
+
+    if (lastTripRoute?.endPoint === stPoint) {
+      return { level: 1, type: 'continued' };
+    }
+    const repeated = this.tripPoints.value.some(t => t.stPoint === stPoint && t.endPoint === endPoint);
+    if (repeated) {
+      return { level: 2, type: 'repeated' };
+    }
+    return { level: 1, type: 'not-continued' };
+
+  }
+
+  getY(level: number): number {
+    return 100 + (level - 1) * 60;
+  }
+  get svgWidth(): number {
+    return (Object.keys(this.cityX).length * this.spacing) + this.padding;
   }
 
 }
